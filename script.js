2027d04e86e05efcb5db1dd155d378bd5e53976a
John Hansuld <jehansuld@gmail.com>
2021-12-24 21:53:11 -0600

    added full code. Not working. not sure if I am trying it correctly. Will troubleshhot more tomorrow

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-const choices = ["Rock", "Paper", "Scissors"]
-const min = Math.ceil(0)
-const max = Math.floor(3)
+const choices = ["ROCK", "PAPER", "SCISSORS"]
 
+// Makes the computers pick
 function computerPlay() {
+    const min = Math.ceil(0)
+    const max = Math.floor(3)
     let rndm = Math.floor(Math.random() * (max - min) + min)
     let computerChoice = choices[rndm]
-    return computerChoice
 }
+// Plays a round
+function playRound(playerSelection, computerChoice) {
+    playerSelection = playerSelection.toCapitalize
+    if (playerSelection === "ROCK") {
+        if (computerChoice === "ROCK") {
+            return "Double rock! That's a tie!"
+        }
+        else if (computerChoice === "PAPER") {
+            return "Paper beats rock, you lose!"
+        }
+        else {
+            return "Rock beats scissors! You win!"
+        }
+    }
+    else if (playerSelection === "PAPER") {
+        if (computerChoice === "PAPER") {
+            return "Double paper! That's a tie!"
+        }
+        else if (computerChoice === "SCISSORS") {
+            return "Scissors beats paper, you lose!"
+        }
+        else {
+            return "Paper beats rock! You win!"
+        }
+    }
+    else if (playerSelection === "SCISSORS") {
+        if (computerChoice === "SCISSORS") {
+            return "Double scissors! That's a tie!"
+        }
+        else if (computerChoice === "ROCK") {
+            return "Rock beats scissors, you lose!"
+        }
+        else {
+            return "Scissors beats paper! You win!"
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
